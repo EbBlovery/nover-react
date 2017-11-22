b29d9ed1a9e89704811b19136e5b736f438e1920
EbBlovery <18217052735@163.com>
2017-11-22 16:14:25 +0800

    add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import RouterMap from './router/routerMap';
+
 class App extends Component {
   render() {
     return (
        <div>
-           sadad
+           <RouterMap />
        </div>
     );
   }
--- src/router/routerMap.jsx
+++ src/router/routerMap.jsx
@@ -1,1 +1,18 @@
-routerMap.jsx
+import React, { Component } from 'react';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+
+import Index from '../component/Index/index';
+
+class RouterMap extends Component {
+	render() {
+		return (
+             <Router>
+             	<Switch>
+             		<Route path="/" component={Index} />
+             	</Switch>
+             </Router>
+		)
+	}
+}
+
+export default RouterMap;
