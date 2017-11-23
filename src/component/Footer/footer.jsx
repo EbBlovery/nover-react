ae038708a59b18b9175200a4d665fbf8cb3a2254
EbBlovery <18217052735@163.com>
2017-11-23 12:56:34 +0800

    footer fin

--- src/component/Footer/footer.jsx
+++ src/component/Footer/footer.jsx
@@ -1,1 +1,32 @@
-footer.jsx
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import './footer.less';
+
+class Footer extends Component {
+	render(){
+		return (
+            <div className="footer">
+            	<div className="footer-search">
+                    <label>
+                    	<input type="text" />
+                    	<button>搜索</button>
+                    </label>
+            	</div>
+            	<div className="footer-navbar">
+	            	<div><Link to="/">首页</Link></div>|
+	            	<div><Link to="/">分类</Link></div>|
+	            	<div><Link to="/">排行</Link></div>|
+	            	<div><Link to="/">书单</Link></div>|
+	            	<div><Link to="/">VIP专区</Link></div>
+            	</div>
+            	<div className="category">
+            		<p>ICP:xxxxxxxxxxxx12号Copynight @ 2017</p>
+            		<p>电话：123123123123</p>
+            	</div>
+            </div>
+		)
+	}
+}
+
+export default Footer;
--- src/component/Index/index.jsx
+++ src/component/Index/index.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Header from '../../container/Header/header';
 import NavBar from '../../container/NavBar/navBar';
 import Content from '../Content/content';
+import Footer from '../Footer/footer';
 
 import './index.less';
 
@@ -15,6 +16,7 @@ class Index extends Component {
              	<div className="containers">
              	    <Content />
              	</div>
+                  <Footer />
              </div>
 		)
 	}
