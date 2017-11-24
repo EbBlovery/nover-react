ed1b98d893365d72a16af0f7ad0da8891c3ff341
EbBlovery <18217052735@163.com>
2017-11-24 14:13:44 +0800

    interset

--- src/container/Book/BookLove/bookLove.jsx
+++ src/container/Book/BookLove/bookLove.jsx
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-import CommendList from '../../NovCommend/CommendList/commendList';
+import './bookLove.less';
 
 class BookLove extends Component {
 	render() {
 		const data = this.props.data;
-		console.log(data)
 		return (
-            <div>
-            	<CommendList data={data}/>
+            <div className="interest-book">
+            	<p><span>你可能感兴趣</span><span style={{color:'red'}}>更多</span></p>
+            	<ul>
+            		{
+            			data && data.map((item,index)=>{
+            				return (
+                                <li key={index}>
+                                	<Link to="/">
+                                        <img src={'http://statics.zhuishushenqi.com' + item.cover}/>
+                                        <p>{item.title}</p>
+                                	</Link>
+                                </li>
+            				)
+            			})
+            		}
+            	</ul>
             </div>
 		)
 	}
--- src/container/NovCommend/CommendList/commendList.jsx
+++ src/container/NovCommend/CommendList/commendList.jsx
@@ -10,15 +10,14 @@ class CommendList extends Component {
 			<ul className="commend-ul">
 				{
 				    data && data.map((item,index)=>{
-				    	console.log(item)
 				        return (
 				            <li className="commend-ul-li" key={index}>
 				                <Link to={{
-				                    pathname:"/book/" + item._id?item._id:'asd',
+				                    pathname:"/book/" + item.book._id,
 				                    state:{data:item}
 				                }}>
-				                    <img src={item.book.cover || '123'}/>
-				                    <p>{item.book.title || '123132'}</p>
+				                    <img src={item.book.cover}/>
+				                    <p>{item.book.title}</p>
 				                </Link>
 				            </li>
 				        )
